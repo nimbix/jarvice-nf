5983ab55d53375e7c7325f9ddebd501131e566a2
Vivek N <rep.movsd@gmail.com>
2023-01-05 17:35:30 +0530

    Removed unwanted files

--- copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sshfs -o IdentityFile=/home/rep/.ssh/google_compute_engine rep@sandbox.us-central1-a.product-engineering-devtest:/data/rep /nextflow
-cp -v /home/rep/projects/nimbix/jarvice-nf/* /nextflow/
-
-ls -la /nextflow
